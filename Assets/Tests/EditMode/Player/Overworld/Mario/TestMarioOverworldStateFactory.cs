bb38c83a2914ca02deec9ad2b838f020216e95c7
DVectors <drvectorartworks@gmail.com>
2024-07-13 08:59:16 +0100

    [ADD] Created unit test for MarioOverworldStateFactory Idle State.

---
+++ Assets/Tests/EditMode/Player/Overworld/Mario/TestMarioOverworldStateFactory.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using NUnit.Framework;
+using Player.Overworld.Mario.States;
+using Tests.EditMode.ObjectFactory;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TestMarioOverworldStateFactory
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void TestMarioOverworldStateFactoryReturnIdle()
+    {
+        //Generate monobehaviour using TestableObjectFactory
+        var playerController = TestableObjectFactory.Create<MarioOverworldPlayerController>();
+
+        var stateFactory = new MarioOverworldStateFactory(playerController);
+
+        var currentState = stateFactory.Idle();
+        
+        Assert.AreEqual(currentState.GetType(), typeof(MarioOverworldIdleState));
+    }
+}
