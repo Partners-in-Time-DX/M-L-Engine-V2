d6d6e02b49e45ba275052348c3538f596078739a
DVectors <drvectorartworks@gmail.com>
2024-08-05 19:04:44 +0100

    [ADD, FIX] Created Used Block script and re imported Used Block model with correct axis settings.

---
+++ Assets/Scripts/Objects/Blocks/UsedBlock.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Objects.Blocks
+{
+    public class UsedBlock : AbstractBlock
+    {
+        protected override bool CheckHit()
+        {
+            return Physics.SphereCast(transform.position, _boxCollider.size.y / 4, Vector3.down, out _, 1, 1 << LayerMask.NameToLayer("Player"));
+        }
+
+        protected override IEnumerator OnHit()
+        {
+            Debug.Log("Used Block hit!");
+            _animator.Play("block_hit");
+            _isHit = false;
+
+            yield return null;
+        }
+    }
+}
