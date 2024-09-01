c894fbda5fac8318ae50dc3e7d98fbbf3205cd55
DVectors <drvectorartworks@gmail.com>
2024-09-01 18:51:46 +0100

    [ADD, FIX, MOD] Added dependent packages, fixed package issues, and modified code that was reliant on partners in time remake logic.

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class Globals
+{
+    public static MarioOverworldPlayerController Mario;
+    
+    public static MusicManager MusicManager;
+    public static SoundManager SoundManager;
+    
+    public static Dictionary<string, ArrayList> LoadCSV(TextAsset file) {
+        
+        Dictionary<string, ArrayList> dictionary = new Dictionary<string, ArrayList>();
+        ArrayList list = new ArrayList();
+        
+        var content = file.text;
+        var lines = content.Split(System.Environment.NewLine);
+
+        for (int i=0; i < lines.Length; i++)
+        {
+            list = new ArrayList();
+            var line = lines[i];
+            if (string.IsNullOrEmpty(line)) continue;
+            string[] values = line.Split('	');
+            for (int j=1; j < values.Length; j++) {
+                list.Add(values[j]);
+            }
+
+            values[0] = new string(values[0].Where(c => !char.IsControl(c)).ToArray());
+            if (values[0] != "") dictionary.Add(values[0], list);
+        }
+
+        return dictionary;
+    }
+}
