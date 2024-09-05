55d0ce0adebfeef2ee95a79ad4aad6fc3f85b972
DVectors <drvectorartworks@gmail.com>
2024-09-05 19:53:53 +0100

    [ADD] Added NavMeshAgent to AbstractOverworldEnemy.cs. This will be used for enemy navigation on a generated navmesh.

--- Assets/Scripts/Enemies/Overworld/Types/AbstractOverworldEnemy.cs
+++ Assets/Scripts/Enemies/Overworld/Types/AbstractOverworldEnemy.cs
@@ -1,6 +1,7 @@
 using System;
 using Enemies.Overworld.States;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Enemies.Overworld.Types
 {
@@ -13,12 +14,16 @@ namespace Enemies.Overworld.Types
         // Transform
         private Vector3 _originalPosition;
         
-        public string name;
+        // Navmesh
+        private NavMeshAgent _agent;
+        
+        public string enemyName;
         public bool canStomp;
         public bool canHammer;
         public float speed = 5;
         
         public EnemyOverworldBaseState CurrentState { get => _currentState; set => _currentState = value; }
+        public Vector3 OriginalPosition { get => _originalPosition; set => _originalPosition = value; }
 
         private void Start()
         {
@@ -29,6 +34,14 @@ namespace Enemies.Overworld.Types
 
             // Store original position for reset state
             _originalPosition = transform.position;
+            
+            // Setup NavMeshAgent
+            _agent = GetComponent<NavMeshAgent>();
+            
+            // Disabling auto-braking allows for continuous movement
+            // between points (ie, the agent doesn't slow down as it
+            // approaches a destination point).
+            _agent.autoBraking = false;
         }
 
         protected override void SetAnimation()
